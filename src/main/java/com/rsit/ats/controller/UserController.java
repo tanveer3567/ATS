b531e62d21cf8c081399352593257fcc1e5b6aa5
tanveer3567 <shaiktanveer.ahmed18@gmail.com>
2020-06-01 22:37:45 -0500

    Added authentication

--- src/main/java/com/rsit/ats/controller/UserController.java
+++ src/main/java/com/rsit/ats/controller/UserController.java
@@ -1,34 +1,49 @@
 package com.rsit.ats.controller;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rsit.ats.config.JwtTokenUtil;
 import com.rsit.ats.model.SystemMessage;
 import com.rsit.ats.model.User;
 import com.rsit.ats.service.UserService;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping("/user")
 public class UserController {
-	
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
 	@Autowired
 	private UserService userService;
 
 	@SuppressWarnings("rawtypes")
-	@PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public SystemMessage addCredentails(@RequestBody User user) {
+	@PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public SystemMessage register(@RequestBody User user) {
 		
-		return userService.addUser(user);
+		return userService.register(user);
 	}
 	
-	@SuppressWarnings("rawtypes")
-	@PostMapping(value = "/validate", produces = MediaType.APPLICATION_JSON_VALUE)
-	public SystemMessage getCredentials(@RequestBody User user) {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@PostMapping(value = "/authenticate", produces = MediaType.APPLICATION_JSON_VALUE)
+	public SystemMessage authenticate(@RequestBody User user) throws Exception {
 		
-		return userService.validate(user);
+		userService.authenticate(user.getUsername(), user.getPassword());
+		final SystemMessage sysMessage = userService.authenticate(user);
+		final String token = jwtTokenUtil.generateToken((UserDetails)sysMessage.getResult());
+		sysMessage.setResult(token);
+		return sysMessage;
 	}
 }
--- src/main/java/com/rsit/ats/service/UserService.java
+++ src/main/java/com/rsit/ats/service/UserService.java
@@ -4,6 +4,7 @@ import static com.rsit.util.CommonUtils.ID;
 import static com.rsit.util.CommonUtils.INTERNAL_SERVER_ERROR;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -15,6 +16,14 @@ import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,16 +32,20 @@ import com.rsit.ats.model.SystemMessage;
 import com.rsit.ats.model.User;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	@Autowired
 	private ElasticHttpClient elasticHttp;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
 
 	@SuppressWarnings({ "unchecked", "rawtypes", "deprecation"})
-	public SystemMessage addUser(User user) {
+	public SystemMessage register(User user) {
 
 		SystemMessage message = null;
 		try {
+			user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
 			IndexRequest request = new IndexRequest("user", "_doc", user.getUsername())
 					.source(new ObjectMapper().writeValueAsString(user), XContentType.JSON);
 			IndexResponse response = elasticHttp.getClient().index(request, RequestOptions.DEFAULT);
@@ -56,38 +69,61 @@ public class UserService {
 		}
 		return message;
 	}
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public SystemMessage validate(User user) {
-
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public SystemMessage authenticate(User user) {
+		
 		SystemMessage message = null;
 		try {
-			GetRequest request = new GetRequest("user", user.getUsername());
-			GetResponse response = elasticHttp.getClient().get(request, RequestOptions.DEFAULT);
-			String source = response.getSourceAsString();
-			if (Objects.nonNull(source)) {
-				message = new SystemMessage<Map<String, String>>();
-				User dbUser = new ObjectMapper().readValue(source, User.class);
-				Map<String, String> map = new HashMap<String, String>();
-				map.put(ID, response.getId());
-				if (dbUser.getPassword().equals(user.getPassword())) {
-					map.put("message", "user validation successful");
-					message.setStatus(200);
-				} else {
-					map.put("message", "user validation failed");
-					message.setStatus(404);
-				}
-				message.setResult(map);
-			} else {
-				message = new SystemMessage<String>();
-				message.setStatus(404);
-				message.setResult("User not found");
-			}
-		} catch (UnsupportedOperationException | IOException e) {
+			message = new SystemMessage<SystemMessage>();
+			user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+			message.setResult(loadUserByUsername(user.getUsername()));
+			message.setStatus(200);
+		} catch (UsernameNotFoundException e) {
+			message = new SystemMessage<String>();
+			message.setStatus(404);
+			message.setResult(e.getMessage());
+		} catch (Exception e) {
 			message = new SystemMessage<String>();
 			message.setStatus(500);
 			message.setResult(INTERNAL_SERVER_ERROR);
 		}
 		return message;
 	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		org.springframework.security.core.userdetails.User user = null;
+		try {
+			GetRequest request = new GetRequest("user", username);
+			GetResponse response = elasticHttp.getClient().get(request, RequestOptions.DEFAULT);
+			String source = response.getSourceAsString();
+			if (Objects.nonNull(source)) {
+				User dbUser = new ObjectMapper().readValue(source, User.class);
+				user = new org.springframework.security.core.userdetails.User(dbUser.getUsername(), dbUser.getPassword(),
+						new ArrayList<>());
+			} else {
+				throw new UsernameNotFoundException("User not found with username: "+username);
+			}
+		} catch (UnsupportedOperationException | IOException e) {
+			throw new UsernameNotFoundException("User not found with username: "+username);
+		}
+		return user;
+	}
+	
+	public void authenticate(String username, String password) throws Exception {
+		
+		Objects.requireNonNull(username);
+		Objects.requireNonNull(password);
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+			System.out.println("ushcuishduiciulhsdilhhn");
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+
 }
